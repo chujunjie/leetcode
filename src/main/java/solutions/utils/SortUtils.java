40deb026cc5ffa225dbe5853cba9ed08873d8bf6
chujunjie <c.junjie@hotmail.com>
2020-07-07 23:30:23 +0800

    桶排序

--- src/main/java/solutions/utils/SortUtils.java
+++ src/main/java/solutions/utils/SortUtils.java
@@ -13,6 +13,45 @@ import java.util.List;
 public class SortUtils {
 
     /**
+     * 桶排序
+     * @param arr arr
+     */
+    private static void bucketSort(int[] arr) {
+
+        // 寻找数组中min，max，用于建桶
+        int min = 0, max = 0;
+        for (int i = 0; i <= arr.length - 1; i++) {
+            if (arr[i] < min) min = arr[i];
+            if (arr[i] > max) max = arr[i];
+        }
+
+        // 开始建桶，注意桶的数量等于max - min + 1
+        int bucketCount = max - min + 1;
+        int[] bucket = new int[bucketCount];
+        for (int i = 0; i <= arr.length - 1; i++)
+            bucket[arr[i] - min]++;
+
+        for (int i = 1; i < bucketCount; i++)
+            bucket[i] = bucket[i] + bucket[i - 1];
+
+        // 开始排序
+        int[] copy = new int[arr.length];
+        System.arraycopy(arr, 0, copy, 0, arr.length);
+
+        // 从后往前排序，保持元素相对位置，保证算法稳定性。
+        for (int i = arr.length - 1; i >= 0; i--) {
+            //桶中i位置元素值即为落入桶i中数组元素在排序后的序列中的下标值
+            int j = --bucket[copy[i] - min];
+            arr[j] = copy[i];
+        }
+
+        // 若从前往后排序，虽然排序结果相同，但会破坏元素相对位置和算法稳定性
+//		for(int i = 0; i <= arr.length - 1; i++){
+//			arr[--bucket[copy[i] - min]] = copy[i];
+//		}
+    }
+
+    /**
      * 冒泡排序
      *
      * @param arr arr
@@ -92,5 +131,10 @@ public class SortUtils {
         int[] arr = {12, 35, 99, 18, 76};
         bubble(arr);
         System.out.println(Arrays.toString(arr));
+
+        // 3.桶排序
+        int[] nums = {-1, -3, 0, 2, 4, 4, 3, 6, 9};
+        bucketSort(nums);
+        System.out.println(Arrays.toString(nums));
     }
 }
