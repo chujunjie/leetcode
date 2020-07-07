823a2ac6a6d81a80a0d74261d3370c6f229ed233
chujunjie <c.junjie@hotmail.com>
2020-07-07 23:37:51 +0800

    堆排序

--- src/main/java/solutions/utils/SortUtils.java
+++ src/main/java/solutions/utils/SortUtils.java
@@ -13,6 +13,69 @@ import java.util.List;
 public class SortUtils {
 
     /**
+     * 堆排序
+     *
+     * @param a a
+     */
+    public static void heap(int[] a) {
+        int len = a.length;
+        // 循环建堆
+        for (int i = 0; i < len - 1; i++) {
+            // 建堆
+            buildMaxHeap(a, len - 1 - i);
+            // 交换堆顶和最后一个元素
+            swap(a, 0, len - 1 - i);
+        }
+    }
+
+    /**
+     * 交换方法
+     *
+     * @param data data
+     * @param i    i
+     * @param j    j
+     */
+    private static void swap(int[] data, int i, int j) {
+        int tmp = data[i];
+        data[i] = data[j];
+        data[j] = tmp;
+    }
+
+    /**
+     * 对data数组从0到lastIndex建大顶堆
+     *
+     * @param data      data
+     * @param lastIndex lastIndex
+     */
+    private static void buildMaxHeap(int[] data, int lastIndex) {
+        // 从lastIndex处节点（最后一个节点）的父节点开始
+        for (int i = (lastIndex - 1) / 2; i >= 0; i--) {
+            // k保存正在判断的节点
+            int k = i;
+            // 如果当前k节点的子节点存在
+            while (k * 2 + 1 <= lastIndex) {
+                // k节点的左子节点的索引
+                int biggerIndex = 2 * k + 1;
+                // 如果biggerIndex小于lastIndex，即biggerIndex+1代表的k节点的右子节点存在
+                if (biggerIndex < lastIndex) {
+                    if (data[biggerIndex] < data[biggerIndex + 1]) {
+                        // biggerIndex总是记录较大子节点的索引
+                        biggerIndex++;
+                    }
+                }
+                // 如果k节点的值小于其较大的子节点的值
+                if (data[k] < data[biggerIndex]) {
+                    swap(data, k, biggerIndex);
+                    // 将biggerIndex赋予k，开始while循环的下一次循环，重新保证k节点的值大于其左右子节点的值
+                    k = biggerIndex;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
      * 桶排序
      *
      * @param arr arr
@@ -137,5 +200,10 @@ public class SortUtils {
         int[] nums = {-1, -3, 0, 2, 4, 4, 3, 6, 9};
         bucket(nums);
         System.out.println(Arrays.toString(nums));
+
+        // 4.堆排序
+        int[] b = {16, 8, 7, 50, 1, 26};
+        heap(b);
+        System.out.println(Arrays.toString(b));
     }
 }
