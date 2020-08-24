3dff396f327573218ef19536c23c6df3b2cc39b5
chujunjie <c.junjie@hotmail.com>
2020-08-25 02:01:28 +0800

    三数之和

---
+++ src/main/java/solutions/twopointer/Solution15.java
@@ -0,0 +1,55 @@
+package solutions.twopointer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+ * <p>
+ * 注意：答案中不可以包含重复的三元组。
+ *
+ * @author chujunjie
+ * @date Create in 1:59 2020/8/25
+ */
+public class Solution15 {
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> lists = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int target = -nums[i];
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                if (nums[left] + nums[right] > target) {
+                    right--;
+                } else if (nums[left] + nums[right] < target) {
+                    left++;
+                } else {
+                    lists.add(Arrays.asList(nums[left], nums[right], -target));
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return lists;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+    }
+}
